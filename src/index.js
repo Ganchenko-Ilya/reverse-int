77f8ae2f1e5ff99bf81c1f73771147832235776d
Ganchenko-Ilya <chicha3d@gmail.com>
2023-01-11 00:21:52 +0300

    Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+
+    let array = String(n).split('');
+    if(array[0] === '-' ){
+        array.shift();
+        return +array.reverse().join('');
+    }
+  return +String(n).split('').reverse().join('')
 }
